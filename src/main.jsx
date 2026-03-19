9fb3d7e71ba0c54b7deb068f9e601bee664917f9
shofiyyahoviii-pixel <shofiyyahoviii@gmail.com>
2026-03-19 09:01:37 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CozyJournal from '../cozy-journal.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CozyJournal />
+  </React.StrictMode>
+)
