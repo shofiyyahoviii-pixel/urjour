d12cd8d9daa754c3bbd5e6b678b63f4861c209df
shofiyyahoviii-pixel <shofiyyahoviii@gmail.com>
2026-03-19 15:49:19 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CozyJournal from '../cozy-journal.jsx'
+import CozyJournal from './cozy-journal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
